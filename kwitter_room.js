675bd79d92b87ac0eefee35bfe7676d254128801
DIBYANSHUMAHARATHI <111274571+DIBYANSHUMAHARATHI@users.noreply.github.com>
2022-08-17 20:13:06 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAiA7_L-7eD8fOIc3uHx0zCH4Ts6V25P68",
+    authDomain: "p-95-73114.firebaseapp.com",
+    databaseURL: "https://p-95-73114-default-rtdb.firebaseio.com",
+    projectId: "p-95-73114",
+    storageBucket: "p-95-73114.appspot.com",
+    messagingSenderId: "730495495462",
+    appId: "1:730495495462:web:4b2473ae9638ba72b565e7",
+    measurementId: "G-NKWRJ9FWLK"
+  };
+  
+  // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
